c25102038a3d317506235e2b4eb1782fd51b12b3
Abhinandan Jain <nandanjain04@gmail.com>
2023-11-15 04:06:41 +0530

    Added basic server structure

--- client/src/components/MaterialCardsWrapper.js
+++ client/src/components/MaterialCardsWrapper.js
@@ -1,58 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import MaterialCard from './MaterialCard';
 import { inventorySave } from './Functions';
 import { Button } from './Buttons';
 import './css/MaterialCardsWrapper.css';
 
 function MaterialCardsWrapper() {
-	const image = [
-		{ src: 'ascension_materials/common/gems/AgnidusAgateGemstone.png', rarity: 5 },
-		{ src: 'ascension_materials/common/gems/AgnidusAgateChunk.png', rarity: 4 },
-		{ src: 'ascension_materials/common/gems/AgnidusAgateFragment.png', rarity: 3 },
-		{ src: 'ascension_materials/common/gems/AgnidusAgateSliver.png', rarity: 2 },
-		
-		{ src: 'ascension_materials/common/gems/NagadusEmeraldGemstone.png', rarity: 5 },
-		{ src: 'ascension_materials/common/gems/NagadusEmeraldChunk.png', rarity: 4 },
-		{ src: 'ascension_materials/common/gems/NagadusEmeraldFragment.png', rarity: 3 },
-		{ src: 'ascension_materials/common/gems/NagadusEmeraldSliver.png', rarity: 2 },
-		
-		{ src: 'ascension_materials/common/gems/PrithivaTopazGemstone.png', rarity: 5 },
-		{ src: 'ascension_materials/common/gems/PrithivaTopazChunk.png', rarity: 4 },
-		{ src: 'ascension_materials/common/gems/PrithivaTopazFragment.png', rarity: 3 },
-		{ src: 'ascension_materials/common/gems/PrithivaTopazSliver.png', rarity: 2 },
-		
-		{ src: 'ascension_materials/common/gems/ShivadaJadeGemstone.png', rarity: 5 },
-		{ src: 'ascension_materials/common/gems/ShivadaJadeChunk.png', rarity: 4 },
-		{ src: 'ascension_materials/common/gems/ShivadaJadeFragment.png', rarity: 3 },
-		{ src: 'ascension_materials/common/gems/ShivadaJadeSliver.png', rarity: 2 },
-		
-		{ src: 'ascension_materials/common/gems/VajradaAmethystGemstone.png', rarity: 5 },
-		{ src: 'ascension_materials/common/gems/VajradaAmethystChunk.png', rarity: 4 },
-		{ src: 'ascension_materials/common/gems/VajradaAmethystFragment.png', rarity: 3 },
-		{ src: 'ascension_materials/common/gems/VajradaAmethystSliver.png', rarity: 2 },
-
-		{ src: 'ascension_materials/common/gems/VarunadaLazuriteGemstone.png', rarity: 5 },
-		{ src: 'ascension_materials/common/gems/VarunadaLazuriteChunk.png', rarity: 4 },
-		{ src: 'ascension_materials/common/gems/VarunadaLazuriteFragment.png', rarity: 3 },
-		{ src: 'ascension_materials/common/gems/VarunadaLazuriteSliver.png', rarity: 2 },
-
-		{ src: 'ascension_materials/common/gems/VayudaTurquoiseGemstone.png', rarity: 5 },
-		{ src: 'ascension_materials/common/gems/VayudaTurquoiseChunk.png', rarity: 4 },
-		{ src: 'ascension_materials/common/gems/VayudaTurquoiseFragment.png', rarity: 3 },
-		{ src: 'ascension_materials/common/gems/VayudaTurquoiseSliver.png', rarity: 2 },
-
-		{ src: 'ascension_materials/normal_drops/WanderersBloomingFlower.png', rarity: 4 },
-		{ src: 'ascension_materials/normal_drops/TreasuredFlower.png', rarity: 3 },
-		{ src: 'ascension_materials/normal_drops/AFlowerYettoBloom.png', rarity: 2 },
-	]
-
-	const inventory = () => {
-		let inventoryList = [];
-		for (let index = 0; index < image.length; index++) {
-			inventoryList.push(
-				<MaterialCard key={index} name={index} defaultValue={0} rarity={image[index].rarity} imageName={image[index].src} />
-			);
+	const [data, setData] = useState([]);
+  
+	useEffect(() => {
+	  	fetchData();
+	}, []);
+  
+	const fetchData = async () => {
+		try {
+			const response = await fetch('http://localhost:3001/api/characters');
+			const result = await response.json();
+			setData(result);
+		} catch (error) {
+			console.error('Error fetching data:', error);
 		}
-		return inventoryList;
 	};
 
 	return (
@@ -66,7 +32,15 @@ function MaterialCardsWrapper() {
 				<div className='flex-body'>
 					<div className='flex-scroll'>
 						<div className='flex-wrapper'>
-							{inventory()}
+							{data.map((item, index) => (
+								<MaterialCard
+									key={index}
+									defaultValue={0}
+									rarity={5}
+									name={item.name}
+									imageName={`characters/${item.name.replace(' ', '')}.png`}
+								/>
+							))}
 						</div>
 					</div>
 				</div>
