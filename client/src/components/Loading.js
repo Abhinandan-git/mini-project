6e736fdcb027c676f91e7536f1cc72d84a816f37
Akansha2503 <akanshakku88@gmail.com>
2023-11-24 11:34:29 +0530

    Bugfix random order data

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -3,8 +3,8 @@ import './css/common.css';
 
 function Loading({ id }) {
 	return (
-		<svg id={id} class="loading" viewBox="0 0 100 100">
-			<circle class="circle" cx="50" cy="50" r="40" />
+		<svg id={id} className="loading" viewBox="0 0 100 100">
+			<circle className="circle" cx="50" cy="50" r="40" />
 		</svg>
 	);
 }
