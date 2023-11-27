5f7fb6c2ae02b1ccb69afd1c8ba4bbba6ba167d5
Abhinandan Jain <nandanjain04@gmail.com>
2023-11-27 23:22:43 +0530

    Updates

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 dotenv.config();
 
 const main_app = express();
-const port = process.env.PORT || 3001;
 const uri = process.env.URL || '';
+const port = process.env.PORT || 3001;
 
 const material_input_app = express();
 const material_port = process.env.MATERIALPORT || 3002;
@@ -41,6 +41,21 @@ main_app.get('/api/material', async (req, res) => {
 material_input_app.post('/api/material-input', async (req, res) => {
 	const recieved_data = req.body;
 	res.status(201).json({ message: 'Data received and processed successfully', data: recieved_data });
+	try {
+		await client.connect();
+		console.log('Connected to MongoDB Atlas');
+		// Connect to database
+		const database = client.db('data');
+		const collection = database.collection('input');
+		// Update documents
+		const updateOperation = { $set: recieved_data };
+		const result = await collection.updateMany(filter, updateOperation);
+		console.log(`${result.modifiedCount} records updated successfully.`);
+	} catch (error) {
+		console.error('Error connecting to MongoDB Atlas', error);
+	} finally {
+		await client.close();
+	}
 });
 
 main_app.listen(port, () => {
