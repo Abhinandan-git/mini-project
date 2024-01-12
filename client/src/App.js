25bf5c76405b30cce189e726ea7fe6768e28e9af
Abhinandan Jain <nandanjain04@gmail.com>
2024-01-12 12:12:58 +0530

    Home page routed

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,15 @@
-import { useState } from 'react';
-import CharacterCardsWrapper from './components/CharacterCardsWrapper';
-import MaterialCardsWrapper from './components/MaterialCardsWrapper';
-import WeaponCardsWrapper from './components/WeaponCardsWrapper';
-import Buttons from './components/Buttons';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
-	const [isInventoryVisible, setIsInventoryVisible] = useState(false);
-	const [isCharacterVisible, setIsCharacterVisible] = useState(false);
-	const [isWeaponVisible, setIsWeaponVisible] = useState(false);
-
-	const toggleInventoryVisibility = () => {
-		setIsInventoryVisible(!isInventoryVisible);
-		if (!isInventoryVisible) {
-			document.getElementById('root').classList.add('noscroll');
-			document.getElementById('inventory-block-wrapper').classList.remove('inventory-block-invis');
-		} else {
-			document.getElementById('root').classList.remove('noscroll');
-			document.getElementById('inventory-block-wrapper').classList.add('inventory-block-invis');
-		}
-	};
-
-	const toggleCharacterVisibility = () => {
-		setIsCharacterVisible(!isCharacterVisible);
-		if (!isCharacterVisible) {
-			document.getElementById('root').classList.add('noscroll');
-			document.getElementById('character-block-wrapper').classList.remove('character-block-invis');
-		} else {
-			document.getElementById('root').classList.remove('noscroll');
-			document.getElementById('character-block-wrapper').classList.add('character-block-invis');
-		}
-	};
-
-	const toggleWeaponVisibility = () => {
-		setIsWeaponVisible(!isWeaponVisible);
-		if (!isWeaponVisible) {
-			document.getElementById('root').classList.add('noscroll');
-			document.getElementById('weapon-block-wrapper').classList.remove('weapon-block-invis');
-		} else {
-			document.getElementById('root').classList.remove('noscroll');
-			document.getElementById('weapon-block-wrapper').classList.add('weapon-block-invis');
-		}
-	};
-
 	return (
-		<>
-			<Buttons toggleFunction={[toggleWeaponVisibility, toggleCharacterVisibility, toggleInventoryVisibility]} />
-			<CharacterCardsWrapper toggleFunction={toggleCharacterVisibility} />
-			<WeaponCardsWrapper toggleFunction={toggleWeaponVisibility} />
-			<MaterialCardsWrapper toggleFunction={toggleInventoryVisibility} />
-		</>
+		<Router>
+			<Routes>
+				<Route path="/" exact element={<Home />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</Router>
 	);
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import CharacterCardsWrapper from './CharacterCardsWrapper';
+import MaterialCardsWrapper from './MaterialCardsWrapper';
+import WeaponCardsWrapper from './WeaponCardsWrapper';
+import Buttons from './Buttons';
+
+function Home() {
+	const [isInventoryVisible, setIsInventoryVisible] = useState(false);
+	const [isCharacterVisible, setIsCharacterVisible] = useState(false);
+	const [isWeaponVisible, setIsWeaponVisible] = useState(false);
+
+	const toggleInventoryVisibility = () => {
+		setIsInventoryVisible(!isInventoryVisible);
+		if (!isInventoryVisible) {
+			document.getElementById('root').classList.add('noscroll');
+			document.getElementById('inventory-block-wrapper').classList.remove('inventory-block-invis');
+		} else {
+			document.getElementById('root').classList.remove('noscroll');
+			document.getElementById('inventory-block-wrapper').classList.add('inventory-block-invis');
+		}
+	};
+
+	const toggleCharacterVisibility = () => {
+		setIsCharacterVisible(!isCharacterVisible);
+		if (!isCharacterVisible) {
+			document.getElementById('root').classList.add('noscroll');
+			document.getElementById('character-block-wrapper').classList.remove('character-block-invis');
+		} else {
+			document.getElementById('root').classList.remove('noscroll');
+			document.getElementById('character-block-wrapper').classList.add('character-block-invis');
+		}
+	};
+
+	const toggleWeaponVisibility = () => {
+		setIsWeaponVisible(!isWeaponVisible);
+		if (!isWeaponVisible) {
+			document.getElementById('root').classList.add('noscroll');
+			document.getElementById('weapon-block-wrapper').classList.remove('weapon-block-invis');
+		} else {
+			document.getElementById('root').classList.remove('noscroll');
+			document.getElementById('weapon-block-wrapper').classList.add('weapon-block-invis');
+		}
+	};
+
+	return (
+		<>
+			<Buttons toggleFunction={[toggleWeaponVisibility, toggleCharacterVisibility, toggleInventoryVisibility]} />
+			<CharacterCardsWrapper toggleFunction={toggleCharacterVisibility} />
+			<WeaponCardsWrapper toggleFunction={toggleWeaponVisibility} />
+			<MaterialCardsWrapper toggleFunction={toggleInventoryVisibility} />
+		</>
+	);
+}
+
+export default Home;
