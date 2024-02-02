dc8c7a83b770f9826c8ea919f998d5e9b0f18c71
Abhinandan Jain <nandanjain04@gmail.com>
2024-02-02 20:36:07 +0530

    Links updated

--- client/src/components/Links.js
+++ client/src/components/Links.js
@@ -1,13 +1,21 @@
 import './css/Links.css';
 import './css/common.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Links() {
+	const [loggedIn, setLoggedIn] = useState(false);
+
+	useEffect(() => {
+		setLoggedIn(true);
+	}, []);
+
 	return (
 		<div className="links-bar">
-			<Link className='sign-link' to="/signup">Sign up</Link>
-			<Link className='sign-link' to="/signin">Sign in</Link>
+			{loggedIn ? <Link className='sign-link' to="/signin">Traveller</Link> : <>
+				<Link className='sign-link logout' to="/signup">Sign up</Link>
+				<Link className='sign-link logout' to="/signin">Sign in</Link>
+			</>}
 		</div>
 	);
 }
